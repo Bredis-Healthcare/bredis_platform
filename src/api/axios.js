e7ce36ba6a03269794ef95287e1aa03041078690
Dain Kim <dikim@bredis.co.kr>
2023-08-16 08:52:05 +0900

    react-toastify 모듈 추가, 에러 핸들링 적용

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,8 +1,34 @@
 import axios from "axios";
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 const instance = axios.create({
     baseURL: "http://15.164.167.59/",
 })
 
+instance.interceptors.response.use(
+    function (response) {
+        return response;
+    },
+    function (error) {
+        if (error.response && error.response.status) {
+            switch (error.response.status) {
+                default:
+                    toast.error(error.response.data.message, {
+                        position: "top-right",
+                        autoClose: 3000,
+                        hideProgressBar: true,
+                        closeOnClick: true,
+                        pauseOnHover: true,
+                        draggable: true,
+                        progress: undefined,
+                        theme: "light",
+                    });
+                    return Promise.reject(error);
+            }
+        }
+        return Promise.reject(error);
+    },
+);
 
 export default instance;
