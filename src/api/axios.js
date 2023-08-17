3f7bd008d628c0785e4659dae6850d273ea6fe44
Dain Kim <dikim@bredis.co.kr>
2023-08-17 12:41:59 +0900

    host https 적용

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const instance = axios.create({
-    baseURL: "http://15.164.167.59/",
+    baseURL: "https://service.bredis.co.kr/",
 })
 
 instance.interceptors.response.use(
