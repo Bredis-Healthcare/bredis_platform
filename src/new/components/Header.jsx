48da19f8ad16d8e1107da4aaadf62499b8cfa3d7
Dain Kim <dikim@bredis.co.kr>
2023-09-18 13:06:17 +0900

    헤더 항목 모두 호버링 효과 추가

--- src/new/components/Header.jsx
+++ src/new/components/Header.jsx
@@ -6,9 +6,26 @@ import {useState} from 'react';
 const Header = () => {
 
     const navigate = useNavigate();
+    const [isHovering1, setIsHovering1] = useState(false);
+    const [isHovering2, setIsHovering2] = useState(false);
     const [isHovering3, setIsHovering3] = useState(false);
     const [isHovering4, setIsHovering4] = useState(false);
 
+    const colorOn1 = () => {
+        setIsHovering1(true);
+    }
+
+    const colorOff1 = () => {
+        setIsHovering1(false);
+    }
+
+    const colorOn2 = () => {
+        setIsHovering2(true);
+    }
+
+    const colorOff2 = () => {
+        setIsHovering2(false);
+    }
     const colorOn3 = () => {
         setIsHovering3(true);
     }
@@ -33,18 +50,22 @@ const Header = () => {
                     <img className=" w-[189px] h-[76px] left-0 top-0 absolute" src="https://cdn.builder.io/api/v1/image/assets%2FTEMP%2F6ef4e97d4d2943759576eb7807cff8ac?&width=800" />
                 </Link>
 
-                <div className=" left-[1099px] top-[32px] absolute text-black text-xl font-bold">서비스 소개</div>
-                <Link to={"/threads"}>
-                    <div className=" left-[1223px] top-[32px] absolute text-black text-xl font-bold">문의하기</div>
+                <Link to={"/"} onMouseOver={colorOn1} onMouseOut={colorOff1}>
+                    <div className={`${isHovering1 ? 'block' : 'hidden'} w-[94px] h-[3px] left-[1099px] top-[16px] absolute bg-sky-900`} />
+                    <div className={`${isHovering1 ? 'text-sky-900' : 'text-black'} left-[1099px] top-[32px] absolute text-black text-xl font-bold`}>서비스 소개</div>
+                </Link>
+                <Link to={"/threads"} onMouseOver={colorOn2} onMouseOut={colorOff2}>
+                    <div className={`${isHovering2 ? 'block' : 'hidden'} w-[82px] h-[3px] left-[1217px] top-[16px] absolute bg-sky-900`} />
+                    <div className={`${isHovering2 ? 'text-sky-900' : 'text-black'} left-[1223px] top-[32px] absolute text-black text-xl font-bold`}>문의하기</div>
                 </Link>
 
                 <Link to={"/orders/create"} onMouseOver={colorOn3} onMouseOut={colorOff3}>
-                    <div className={`${isHovering3 ? 'block' : 'hidden'} w-[85px] h-[3px] left-[1320px] top-[16px] absolute bg-sky-900`} />
+                    <div className={`${isHovering3 ? 'block' : 'hidden'} w-[82px] h-[3px] left-[1320px] top-[16px] absolute bg-sky-900`} />
                     <div className={`${isHovering3 ? 'text-sky-900' : 'text-black'} left-[1326px] top-[32px] absolute text-xl font-bold`}>주문하기</div>
                 </Link>
 
                 <Link to={"/orders/list"} onMouseOver={colorOn4} onMouseOut={colorOff4}>
-                    <div className={`${isHovering4 ? 'block' : 'hidden'} w-[85px] h-[3px] left-[1415px] top-[16px] absolute bg-sky-900`} />
+                    <div className={`${isHovering4 ? 'block' : 'hidden'} w-[82px] h-[3px] left-[1415px] top-[16px] absolute bg-sky-900`} />
                     <div className={`${isHovering4 ? 'text-sky-900' : 'text-black'} left-[1421px] top-[32px] absolute  text-xl font-bold`}>주문내역</div>
                 </Link>
                 <div className=" w-[50px] h-[30px] left-[1521px] top-[29px] absolute">
