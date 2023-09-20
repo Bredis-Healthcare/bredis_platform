a55ae308ea0f2b80cad8c1d233f2d300595e3c18
Dain Kim <dikim@bredis.co.kr>
2023-09-20 16:08:28 +0900

    테스트용 로그아웃 간단 구현

--- src/new/components/Header.jsx
+++ src/new/components/Header.jsx
@@ -1,6 +1,8 @@
 import {Link, useNavigate} from "react-router-dom";
 import * as React from 'react';
 import {useState} from 'react';
+import axios from "../../api/axios";
+import {useCookies} from "react-cookie";
 
 
 const Header = () => {
@@ -10,6 +12,9 @@ const Header = () => {
     const [isHovering2, setIsHovering2] = useState(false);
     const [isHovering3, setIsHovering3] = useState(false);
     const [isHovering4, setIsHovering4] = useState(false);
+    const [profileMenuOn, setProfileMenuOn] = useState(false);
+
+    const [cookies, setCookie, removeCookie] = useCookies(['login']);
 
     const colorOn1 = () => {
         setIsHovering1(true);
@@ -40,6 +45,17 @@ const Header = () => {
 
     const colorOff4 = () => {
         setIsHovering4(false);
+    }
+
+    function clickProfileIcon() {
+        setProfileMenuOn(profileMenuOn => !profileMenuOn)
+    }
+
+    async function logout() {
+        const request = await axios.post('logout')
+        removeCookie(['login']);
+        clickProfileIcon()
+        navigate("/");
     }
 
     return (
@@ -73,11 +89,16 @@ const Header = () => {
                         <img className="object-cover object-center" src="https://cdn.builder.io/api/v1/image/assets%2FTEMP%2Fec38e501943d468fa0df8b6f1a34a36f?&width=200" alt=""/>
                     </div>
                 </div>
-                <div className=" w-[49px] h-[30px] left-[1581px] top-[29px] absolute">
+                <button className=" w-[49px] h-[30px] left-[1581px] top-[29px] absolute"
+                        onClick={() => clickProfileIcon()}>
                     <div className="UserCicrleLight w-[55px] h-[30px] left-0 top-0 absolute">
                         <img className="object-cover object-center" src="https://cdn.builder.io/api/v1/image/assets%2FTEMP%2F620ec0bc269049aa87c29d8e1cbbc3be?&width=200" alt=""/>
                     </div>
-                </div>
+                </button>
+                <button className={`${profileMenuOn ? 'block' : 'hidden'} absolute w-auto left-[1515px] top-[58px] z-10`}
+                        onClick={() => logout()}>
+                    <img className="object-cover object-center" src="https://cdn.builder.io/api/v1/image/assets/TEMP/bc18ab58-4f37-43ec-8a4c-32e441c85efb?&width=800" />
+                </button>
             </div>
     );
 }
