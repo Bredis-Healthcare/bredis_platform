83a5c3b9752d8e1a6967c9461704722f0ff85178
Dain Kim <dikim@bredis.co.kr>
2023-10-05 16:32:27 +0900

    고객 목록 화면에 상태 알럿 추가

--- src/new/pages/admin/AdminMembersList.jsx
+++ src/new/pages/admin/AdminMembersList.jsx
@@ -31,16 +31,35 @@ function AdminMembersList() {
                                 <div key={member.id}>
                                     <div className=" w-[1066px] h-[160px] left-[77px] top-[0px] my-[8px] relative">
                                         <div className=" left-0 top-[56px] absolute text-black text-2xl font-bold font-['Inter']">{member.organization}</div>
-                                        <div className="1 left-[559px] top-[21px] absolute text-neutral-800 text-lg font-normal font-['Inter']">답변하지 않은 문의 1건</div>
                                         <Link to="/admin/members/detail" state={{resourceId: member.id}}>
                                             <button className=" w-[120px] h-[35px] left-[946px] top-[45px] absolute">
                                                 <div className="Rectangle7 w-[120px] h-[35px] left-0 top-0 absolute bg-neutral-100 rounded-[9px] border-2 border-slate-500" />
                                                 <div className=" w-[93px] h-[17px] left-[19px] top-[6px] absolute text-slate-500 text-lg font-bold font-['Inter']">상세 보기 ></div>
                                             </button>
                                         </Link>
-                                        <div className=" left-[456px] top-[21px] absolute text-neutral-800 text-lg font-bold font-['Inter']">문의 상태</div>
-                                        <div className=" left-[456px] top-[64px] absolute text-neutral-800 text-lg font-bold font-['Inter']">주문 상태</div>
-                                        <div className="1214 left-[559px] top-[64px] absolute text-neutral-800 text-lg font-normal font-['Inter']">견적 확인 요청 1건<br/>주문 진행 2건 | 답변하지 않은 문의 1건<br/>분석 완료 4건</div>
+                                        <div className=" left-[456px] top-[21px] absolute text-neutral-800 text-lg font-medium font-['Inter']">문의 상태</div>
+                                        <div className=" left-[456px] top-[100px] absolute text-neutral-800 text-lg font-medium font-['Inter']">주문 상태</div>
+                                        <div className=" left-[456px] top-[64px] absolute text-neutral-800 text-lg font-medium font-['Inter']">견적 요청</div>
+                                        <div className="left-[559px] top-[21px] absolute text-neutral-800 text-lg font-light font-['Inter']">
+                                            <div className={"inline-block"}>읽지 않은 문의 {member.unreadMessages}건</div>
+                                            <div className={`${member.unreadMessages > 0 ? 'inline-block' : 'hidden'} Ellipse49 w-3 h-3 left-[2px] top-[-7px] relative bg-red-600 rounded-full`} />
+                                        </div>
+
+                                        <div className="1214 left-[559px] top-[64px] absolute text-neutral-800 text-lg font-light font-['Inter']">
+                                            고객 응답 대기 {member.quotationRequestWaitingCustomerCount}건
+                                            <div className={`mx-[4px] ${member.quotationRequestWaitingAdminCount > 0 ? 'inline-block' : 'hidden'}`}>
+                                                | 확인 필요 {member.quotationRequestWaitingAdminCount}건
+                                                <div className={ `inline-block Ellipse49 w-3 h-3 left-[2px] top-[-7px] relative bg-red-600 rounded-full`} />
+                                            </div>
+                                        </div>
+                                        <div className="1214 left-[559px] top-[100px] absolute text-neutral-800 text-lg font-light font-['Inter']">
+                                            주문 진행 {member.inProgressOrderCount}건
+                                            <div className={`mx-[4px] ${member.unreadMessages > 0 ? 'inline-block' : 'hidden'}`}>
+                                                | 읽지 않은 문의 {member.unreadOrderMessages}건
+                                                <div className={ `inline-block Ellipse49 w-3 h-3 left-[2px] top-[-7px] relative bg-red-600 rounded-full`} />
+                                            </div>
+                                            <br/>분석 완료 {member.completeOrderCount}건
+                                        </div>
                                         <div className=" left-0 top-[93px] absolute text-black text-lg font-normal font-['Inter']">{member.name} {member.position}</div>
                                         <div className="20230831143000 left-0 top-0 absolute text-zinc-500 text-base font-normal font-['Inter']"><br/>최종 커뮤니케이션 일시: 2023.08.31 14:30:00</div>
                                         <div className="TestGmailCom left-[113px] top-[93px] absolute text-black text-lg font-normal font-['Inter']">{member.email}</div>
