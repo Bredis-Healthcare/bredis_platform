d1b7b831a9f3f0e9db7fef2e903b2b01d3f06b3d
Dain Kim <dikim@bredis.co.kr>
2023-10-06 17:52:48 +0900

    디자인 다듬기

--- src/new/pages/OrderList.jsx
+++ src/new/pages/OrderList.jsx
@@ -47,7 +47,7 @@ function OrderList() {
                         {
                             data.orderItems.length > 0 ?
                                 <div
-                                    className="Contents shadow-[0px_0px_4px_2px_rgba(0,0,0,0.25)] w-full max-w-[1222px] self-center flex flex-col ml-[0.5px] mt-[46px] px-[20px] pt-[40px]">
+                                    className="Contents shadow-[0px_0px_4px_2px_rgba(0,0,0,0.25)] w-full max-w-[1222px] self-center flex flex-col ml-[0.5px] mt-[46px] pb-[50px] px-[20px] pt-[40px]">
                                     {
                                         data.orderItems.map((order, index) => (
                                             <div key={order.id}>
@@ -58,17 +58,32 @@ function OrderList() {
                                                         <div className="h-5"></div>
                                                     </div>
                                                 )}
-                                                <div className={`w-[1077px] h-[200px] left-[76px] top-[${34 + index * 200}px] relative`}>
-                                                    <div className="SimoaPtau181AdvantageV214 left-0 top-[30px] max-w-[580px] absolute text-black text-2xl font-bold font-['Inter']">{order.title}</div>
-                                                    <div className="Ab40Ab42GfapNflPtau181 left-0 top-[121px] absolute text-black text-2xl font-normal font-['Inter']">{order.requestDetail}</div>
-                                                    <div className="987204 left-0 top-0 absolute text-zinc-500 text-lg font-normal font-['Inter']">주문번호: {order.orderNumber}</div>
-                                                    <div className=" left-[726px] top-[1px] absolute text-zinc-500 text-lg font-normal font-['Inter']">주문일시</div>
-                                                    <div className="0503 left-[650px] top-[31px] absolute text-black text-2xl font-light font-['Inter']">{order.createdDatetime}</div>
-                                                    <div className=" left-[970px] top-[1px] absolute text-zinc-500 text-lg font-normal font-['Inter']">상태</div>
-                                                    <div className=" left-[950px] top-[31px] absolute text-lime-500 text-2xl font-bold font-['Inter']">{order.status}</div>
+                                                <div className={`w-[1077px] h-[200px] left-[76px] top-[34px] relative`}>
+                                                    <div className="left-0 top-[30px] max-w-[580px] absolute text-black text-2xl font-bold font-['Inter']">{order.title}</div>
+                                                    <p className="left-0 top-[121px] w-[600px] absolute text-black text-lg font-normal font-['Inter'] truncate ...">{order.requestDetail}</p>
+                                                    <div className="left-0 top-0 absolute text-zinc-500 text-lg font-normal font-['Inter']">주문번호: {order.orderNumber}</div>
+                                                    <div className=" left-[630px] top-[1px] absolute flex flex-col object-center">
+                                                        <div className="flex flex-col text-center text-zinc-500 text-lg font-normal font-['Inter'] ">주문일시</div>
+                                                        <div className="flex flex-col text-center text-black text-2xl font-light font-['Inter']">{order.createdDatetime}</div>
+                                                        <div className="flex flex-col text-center text-zinc-500 text-lg font-normal font-['Inter'] mt-[25px]">금액</div>
+                                                        <div className="flex flex-col text-center text-black text-2xl font-medium font-['Inter']">₩{order.price}</div>
+                                                    </div>
+
+                                                    <div className="left-[900px] top-[1px] absolute flex flex-col object-center">
+                                                        <div className="flex flex-col text-center text-zinc-500 text-lg font-normal font-['Inter']">상태</div>
+                                                        <div className="flex flex-col text-center w-[150px] text-lime-500 text-2xl font-bold font-['Inter']">{order.status}</div>
+                                                        <div className="flex flex-col">
+                                                            <Link to="/orders/detail" state={{resourceId: order.orderNumber}}>
+                                                                <button className="mx-[20px] w-[120px] h-[35px] mt-[40px] relative">
+                                                                    <div className="Rectangle7 w-[120px] h-[35px] left-0 top-0 absolute bg-neutral-100 rounded-[9px] border-2 border-slate-500"/>
+                                                                    <div className=" w-[90px] h-[17px] left-[19px] top-[6px] absolute text-slate-500 text-lg font-bold font-['Inter']">주문 상세 ></div>
+                                                                </button>
+                                                            </Link>
+                                                        </div>
+                                                    </div>
+
                                                     <div className=" left-0 top-[88px] absolute text-zinc-500 text-lg font-normal font-['Inter']">의뢰내용</div>
-                                                    <div className=" left-[745px] top-[88px] absolute text-zinc-500 text-lg font-normal font-['Inter']">금액</div>
-                                                    <div className="38262070 left-[700px] top-[121px] absolute text-black text-2xl font-medium font-['Inter']">₩{order.price}</div>
+
                                                     {
                                                         order.unreadAdminMessages > 0 ? (
                                                             <div className="UnreadNoti">
@@ -77,12 +92,7 @@ function OrderList() {
                                                             </div>
                                                         ) : (<div />)
                                                     }
-                                                    <Link to="/orders/detail" state={{resourceId: order.orderNumber}}>
-                                                        <button className=" w-[120px] h-[35px] left-[935px] top-[110px] absolute">
-                                                            <div className="Rectangle7 w-[120px] h-[35px] left-0 top-0 absolute bg-neutral-100 rounded-[9px] border-2 border-slate-500"/>
-                                                            <div className=" w-[90px] h-[17px] left-[19px] top-[6px] absolute text-slate-500 text-lg font-bold font-['Inter']">주문 상세 ></div>
-                                                        </button>
-                                                    </Link>
+
                                                 </div>
                                             </div>
                                         ))
