69381865d327d365a73241252e50e8ccaf6a8707
Dain Kim <dikim@bredis.co.kr>
2023-10-10 16:42:11 +0900

    문의하기 버튼 위치 조정

--- src/new/components/AskButton.jsx
+++ src/new/components/AskButton.jsx
@@ -4,7 +4,7 @@ function AskButton () {
     
     return (
         <Link to={"/threads/list"}>
-            <div className=" w-[106px] h-[106px] fixed right-[20%] bottom-[10%]">
+            <div className=" w-[106px] h-[106px] fixed right-[5%] bottom-[12%]">
                 <div className=" w-[100%] h-[100%] left-0 top-0 absolute bg-neutral-100 rounded-full shadow"/>
                 <div className=" w-[95%] h-[95%] left-[2.5%] top-[2.5%] absolute bg-yellow-300 rounded-full"/>
                 <div className=" w-[100%] top-[35%] absolute text-zinc-700 text-[22px] font-bold text-center align-middle transition-all hover:scale-[1.1] ">문의하기</div>
