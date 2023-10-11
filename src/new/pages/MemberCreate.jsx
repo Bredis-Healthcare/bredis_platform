5abf071074b9b16b8cff754f6a1b7a6301bed35e
Dain Kim <dikim@bredis.co.kr>
2023-10-11 17:41:00 +0900

    회원가입에 이용약관, 개인정보처리방침 동의 추가

--- src/new/pages/MemberCreate.jsx
+++ src/new/pages/MemberCreate.jsx
@@ -14,11 +14,15 @@ function MemberCreate() {
         let organization = document.getElementById('organization').value
         let department = document.getElementById('department').value
         let position = document.getElementById('position').value
+        let agree1 = document.getElementById('checkbox1').checked
+        let agree2 = document.getElementById('checkbox2').checked
+        let agree3 = document.getElementById('checkbox3').checked
         if (!name) {alert("이름을 입력해주세요.");return}
         if (!email) {alert("이메일을 입력해주세요.");return}
         if (!password) {alert("비밀번호를 입력해주세요.");return}
         if (password !== passwordConfirm) {alert("비밀번호를 확인해주세요.");return}
         if (!mobile) {alert("전화번호를 입력해주세요.");return}
+        if (!agree1 || !agree2 || !agree3) {alert("모든 약관에 동의해주셔야 회원가입이 가능합니다."); return}
 
         await axios.post(`/register`, { "email": email, "password": password, "name": name, "mobile": mobile, "organization": organization, "department": department, "position": position});
         alert("브레디스헬스케어 연구분석서비스의 회원이 되신 것을 환영합니다.")
@@ -26,7 +30,7 @@ function MemberCreate() {
     }
 
     return <Layout menuName="회원가입">
-        <div className="2 w-[1667px] h-[955px] relative bg-neutral-100">
+        <div className="2 w-[1667px] h-[1100px] relative bg-neutral-100">
             <div className=" w-[90px] h-[23px] left-[470px] top-[20px] relative text-black text-base font-bold font-['Inter'] mt-10">이름</div>
             <input id="name" rows="1" className="resize-none left-[470px] top-[30px] relative block p-2.5 mb-3 w-[633px] text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="홍길동"></input>
 
@@ -51,6 +55,26 @@ function MemberCreate() {
             <div className=" w-[90px] h-[23px] left-[470px] top-[20px] relative text-black text-base font-bold font-['Inter'] mt-10">직책</div>
             <input id="position" rows="1" className="resize-none left-[470px] top-[30px] relative block p-2.5 mb-3 w-[633px] text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="연구원"></input>
 
+            <div className="left-[450px] relative mt-[50px]">
+                <input type="checkbox" id="checkbox1" className="accent-slate-500 align-middle ml-[1.5rem] mr-[6px] h-[1.35rem] w-[1.35rem] "/>
+                <label className="align-middle text-black text-base font-normal font-['Inter'] inline-block pl-[0.15rem] hover:cursor-pointer" htmlFor="checkbox1">
+                    이용약관 동의
+                </label>
+                <a href="/terms/service" target="_blank" className="align-middle text-slate-500 underline text-base font-normal font-['Inter'] inline-block pl-[0.3rem] hover:cursor-pointer">보기</a>
+            </div>
+            <div className="left-[450px] relative mt-[10px]">
+                <input type="checkbox" id="checkbox2" className="accent-slate-500 align-middle ml-[1.5rem] mr-[6px] h-[1.35rem] w-[1.35rem] "/>
+                <label className="align-middle text-black text-base font-normal font-['Inter'] inline-block pl-[0.15rem] hover:cursor-pointer" htmlFor="checkbox2">
+                    개인정보 취급방침 동의
+                </label>
+                <a href="/terms/privacy" target="_blank" className="align-middle text-slate-500 underline text-base font-normal font-['Inter'] inline-block pl-[0.3rem] hover:cursor-pointer">보기</a>
+            </div>
+            <div className="left-[450px] relative mt-[10px]">
+                <input type="checkbox" id="checkbox3" className="accent-slate-500 align-middle ml-[1.5rem] mr-[6px] h-[1.35rem] w-[1.35rem] "/>
+                <label className="align-middle text-black text-base font-normal font-['Inter'] inline-block pl-[0.15rem] hover:cursor-pointer" htmlFor="checkbox3">
+                    만 14세 이상입니다.
+                </label>
+            </div>
 
             <button style={{left: 'calc(50% - 120px)'}} className=" w-[126px] h-[35px] top-[50px] mx-2 my-2 relative" onClick={() => submit()}>
                 <div className="Rectangle7 w-[120px] h-[35px] left-0 top-0 absolute bg-slate-500 rounded-[9px]" />
