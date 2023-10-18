17a8aa06b431f8b412a3b416867e1c3577a85fa7
ziiiiimen <kim1jeahun@naver.com>
2023-10-18 15:19:56 +0900

    회원가입 정보 확인, 이메일 확인

--- src/new/pages/MemberCreate.jsx
+++ src/new/pages/MemberCreate.jsx
@@ -1,59 +1,78 @@
 import Layout from "../components/Layout";
-import React from "react";
+import React, { useEffect, useState } from "react";
 import axios from "../../api/axios";
 import {useNavigate} from "react-router-dom";
 
 function MemberCreate() {
     const navigate = useNavigate();
+    const [name, setName] = useState('');
+    const [email, setEmail] = useState('');
+    const [isValidEmail, setIsValidEmail] = useState(true);
+    const [password, setPassword] = useState('');
+    const [confirmPassword, setConfirmPassword] = useState('');
+    const [mobile, setMobile] = useState('');
+    const [organization, setOrganization] = useState('');
+    const [department, setDepartment] = useState('');
+    const [position, setPosition] = useState('');
+    const [agree1, setAgree1] = useState(false);
+    const [agree2, setAgree2] = useState(false);
+    const [agree3, setAgree3] = useState(false);
+    useEffect(() => {
+        const emailRegex = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/;
+        setIsValidEmail(emailRegex.test(email));
+    }, [email]);
+
     async function submit() {
-        let name = document.getElementById('name').value
-        let email = document.getElementById('email').value
-        let password = document.getElementById('password').value
-        let passwordConfirm = document.getElementById('passwordConfirm').value
-        let mobile = document.getElementById('mobile').value
-        let organization = document.getElementById('organization').value
-        let department = document.getElementById('department').value
-        let position = document.getElementById('position').value
-        let agree1 = document.getElementById('checkbox1').checked
-        let agree2 = document.getElementById('checkbox2').checked
-        let agree3 = document.getElementById('checkbox3').checked
         if (!name) {alert("이름을 입력해주세요.");return}
         if (!email) {alert("이메일을 입력해주세요.");return}
+        if (isValidEmail) {alert("이메일을 확인해 주세요.");return}
         if (!password) {alert("비밀번호를 입력해주세요.");return}
-        if (password !== passwordConfirm) {alert("비밀번호를 확인해주세요.");return}
+        if (password !== confirmPassword) {alert("비밀번호를 확인해주세요.");return}
         if (!mobile) {alert("전화번호를 입력해주세요.");return}
         if (!agree1 || !agree2 || !agree3) {alert("모든 약관에 동의해주셔야 회원가입이 가능합니다."); return}
 
-        await axios.post(`register`, { "email": email, "password": password, "name": name, "mobile": mobile, "organization": organization, "department": department, "position": position});
-        alert("브레디스헬스케어 연구분석서비스의 회원이 되신 것을 환영합니다.")
+        const request = await axios.post(`register`, { email, password, name, mobile, organization, department, position });
+        if(request !== "오류" ){
+            alert("브레디스헬스케어 연구분석서비스의 회원이 되신 것을 환영합니다.")
+        }
+        else{
+            alert("이미 가입된 이메일입니다.")
+        }
         navigate("/");
     }
+    
 
     return <Layout menuName="회원가입">
         <div className="2 w-[1667px] h-[1100px] relative bg-neutral-100">
             <div className=" w-[90px] h-[23px] left-[470px] top-[20px] relative text-black text-base font-bold font-['Inter'] mt-10">이름</div>
-            <input id="name" rows="1" className="resize-none left-[470px] top-[30px] relative block p-2.5 mb-3 w-[633px] text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="홍길동"></input>
+            <input id="name" rows="1" value={name} onChange={(e) => setName(e.target.value)}  className="resize-none left-[470px] top-[30px] relative block p-2.5 mb-3 w-[633px] text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="홍길동"></input>
 
             <div className=" w-[90px] h-[23px] left-[470px] top-[20px] relative text-black text-base font-bold font-['Inter'] mt-10">이메일</div>
-            <input id="email" type="email" rows="1" className="resize-none left-[470px] top-[30px] relative block p-2.5 mb-3 w-[633px] text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="example@bredis.co.kr"></input>
-
+            <input id="email" type="email" rows="1" value={email} onChange={(e) => setEmail(e.target.value)} className="resize-none left-[470px] top-[30px] relative block p-2.5 mb-3 w-[633px] text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="example@bredis.co.kr"></input>
+            {!isValidEmail && email && 
+                <p className="left-[470px] top-[20px] relative text-red-500 text-sm h-[20px]">
+                    {"이메일 형식이 올바르지 않습니다."}
+                </p>
+            }
             <div className=" w-[90px] h-[23px] left-[470px] top-[20px] relative text-black text-base font-bold font-['Inter'] mt-10">비밀번호</div>
-            <input id="password" type="password" rows="1" className="resize-none left-[470px] top-[30px] relative block p-2.5 mb-3 w-[633px] text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="비밀번호를 입력해주세요."></input>
+            <input id="password" type="password" rows="1" value={password} onChange={(e) => setPassword(e.target.value)}  className="resize-none left-[470px] top-[30px] relative block p-2.5 mb-3 w-[633px] text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="비밀번호를 입력해주세요."></input>
 
             <div className=" w-[90px] h-[23px] left-[470px] top-[20px] relative text-black text-base font-bold font-['Inter'] mt-10">비밀번호 확인</div>
-            <input id="passwordConfirm" type="password" rows="1" className="resize-none left-[470px] top-[30px] relative block p-2.5 mb-3 w-[633px] text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="비밀번호를 입력해주세요."></input>
-
+            <input id="passwordConfirm" type="password" value={confirmPassword} onChange={(e) => setConfirmPassword(e.target.value)} rows="1" className="resize-none left-[470px] top-[30px] relative block p-2.5 mb-3 w-[633px] text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="비밀번호를 다시 입력해주세요"></input>
+            <p className="left-[470px] top-[20px] relative  text-red-500 text-sm h-[20px]">
+                {confirmPassword && (password === confirmPassword ? '비밀번호가 일치합니다.' : '비밀번호가 일치하지 않습니다.')}
+            </p>
             <div className=" w-[90px] h-[23px] left-[470px] top-[20px] relative text-black text-base font-bold font-['Inter'] mt-10">전화번호</div>
-            <input id="mobile" type="mobile" rows="1" className="resize-none left-[470px] top-[30px] relative block p-2.5 mb-3 w-[633px] text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="010-1234-1234"></input>
-
+            <input id="mobile" type="mobile" value={mobile} onChange={(e) => setMobile(e.target.value)} rows="1" className="resize-none left-[470px] top-[30px] relative block p-2.5 mb-3 w-[633px] text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="010-1234-1234"></input>
+            
             <div className=" w-[90px] h-[23px] left-[470px] top-[20px] relative text-black text-base font-bold font-['Inter'] mt-10">조직</div>
-            <input id="organization" rows="1" className="resize-none left-[470px] top-[30px] relative block p-2.5 mb-3 w-[633px] text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="브레디스헬스케어"></input>
+            <input id="organization" rows="1" value={organization} onChange={(e) => setOrganization(e.target.value)} className="resize-none left-[470px] top-[30px] relative block p-2.5 mb-3 w-[633px] text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="브레디스헬스케어"></input>
 
             <div className=" w-[90px] h-[23px] left-[470px] top-[20px] relative text-black text-base font-bold font-['Inter'] mt-10">부서</div>
-            <input id="department" rows="1" className="resize-none left-[470px] top-[30px] relative block p-2.5 mb-3 w-[633px] text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="R&D팀"></input>
+            <input id="department" rows="1"  value={department} onChange={(e) => setDepartment(e.target.value)} className="resize-none left-[470px] top-[30px] relative block p-2.5 mb-3 w-[633px] text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="R&D팀"></input>
 
             <div className=" w-[90px] h-[23px] left-[470px] top-[20px] relative text-black text-base font-bold font-['Inter'] mt-10">직책</div>
-            <input id="position" rows="1" className="resize-none left-[470px] top-[30px] relative block p-2.5 mb-3 w-[633px] text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="연구원"></input>
+            <input id="position" rows="1" value={position} onChange={(e) => setPosition(e.target.value)} className="resize-none left-[470px] top-[30px] relative block p-2.5 mb-3 w-[633px] text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="연구원"></input>
 
             <div className="left-[450px] relative mt-[50px]">
                 <input type="checkbox" id="checkbox1" className="accent-slate-500 align-middle ml-[1.5rem] mr-[6px] h-[1.15rem] w-[1.15rem] "/>
