758ed4611cf05c1260a9e718d19c25cf497c9c26
ziiiiimen <kim1jeahun@naver.com>
2023-10-18 10:37:07 +0900

    useDebounce 훅 추가

---
+++ src/new/components/useDebounce.js
@@ -0,0 +1,26 @@
+import { useState, useEffect } from "react";
+
+export const useDebounce = (value, delay) => {
+  const [debounceValue, setDebounceValue] = useState(value);
+
+  useEffect(() => {
+    const handler = setTimeout(() => {
+      setDebounceValue(value);
+    }, delay);
+
+    return () => {
+      clearTimeout(handler);
+    };
+  }, [value, delay]);
+
+  return debounceValue;
+};
+
+// 사용 예시 
+// searchTerm이 500ms 동안 변하지 않을때 fetch 할때
+// const debouncedSearchTerm = useDebounce(searchTerm, 500);
+// useEffect(() => {
+//   if (debouncedSearchTerm) {
+//     fetchSearchMovie(debouncedSearchTerm);
+//   }
+// }, [debouncedSearchTerm]);
