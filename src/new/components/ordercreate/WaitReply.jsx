6b4f3febf911b66bc5feffc6a0b589207055be00
Dain Kim <dikim@bredis.co.kr>
2023-10-20 14:56:10 +0900

    체크 아이콘 자체 공급 및 배경 제거

--- src/new/components/ordercreate/WaitReply.jsx
+++ src/new/components/ordercreate/WaitReply.jsx
@@ -1,8 +1,10 @@
+import checkIcon from "../../../img/check_icon.png"
+
 function WaitReply () {
     return (
         <div className="top-[250px] h-[1000px] relative">
-            <img src="https://cdn.builder.io/api/v1/image/assets/TEMP/6b981ecb-b1b4-4b07-822c-4628885bfdf8?&width=1200"
-                 className="aspect-[1.44] object-cover object-center max-w-[452px] flex shrink-0 ml-auto mr-auto" />
+            <img src={checkIcon}
+                 className="object-cover object-center max-w-[452px] flex ml-auto mr-auto" />
             <div className="text-[#888988] text-center not-italic font-bold text-[27px] self-center flex flex-col ml-[40px] mt-[134px]">
                 견적 요청서가 정상적으로 접수되었습니다.<br/>영업일 기준 3일 내에 담당자가 회신을 드릴 예정입니다.
             </div>
