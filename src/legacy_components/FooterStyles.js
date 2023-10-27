3ba470bd0d99ac8e2377c32a41e3800d5be510a9
Dain Kim <dikim@bredis.co.kr>
2023-10-27 11:59:39 +0900

    레거시 컴포넌트 삭제

--- src/legacy_components/FooterStyles.js
+++
@@ -1,9 +0,0 @@
-import styled from 'styled-components';
-
-export const FooterContainer = styled.footer`
-    padding: 20px;
-    background-color: #f7f7f7;
-    text-align: center;
-    font-size: 14px;
-    margin-top: 50px; // Adjust as needed
-`;
